3940f3a77bf27f3937a03b0e2b3d7e5ce3579297
Felipe Rosene Klapsztein <feliperosenek@gmail.com>
2022-10-12 20:02:27 -0300

    deploy

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const  Server  = require("socket.io");
+const io = new Server(server);
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+    var data = req.body
+    console.log(data)
+    io.emit('log', data.message);      
+ 
+    res.send('ok')
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('listening on *:3000');
+});
